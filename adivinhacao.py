839d8019533b984f31d578d4e0a4f6ef04cb35aa
Lucas Victor <lvitors@hotmail.com>
2020-03-10 08:03:22 -0300

    Incluindo tentativas para o usuário

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,21 +3,27 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite um número: ")
-print("Você digitou o numero:", chute_str)
+while(rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_str = input("Digite um número: ")
+    print("Você digitou o numero:", chute_str)
 
-chute = int(chute_str)
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-if(acertou):
-    print("Você acertou! xD")
-else:
-    if(maior):
-        print("Você errou! O seu número é maior do que o número secreto. =(")
-    elif(menor):
-        print("Você errou! O seu número é menor do que o número secreto. =(")
+    if(acertou):
+        print("Você acertou! xD")
+    else:
+        if(maior):
+            print("Você errou! O seu número é maior do que o número secreto. =(")
+        elif(menor):
+            print("Você errou! O seu número é menor do que o número secreto. =(")
+
+    rodada = rodada + 1
 
 print("Fim do jogo!")
