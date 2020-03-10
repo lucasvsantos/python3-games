9b9ac80fd5c3e003be8b985fb9381f680b4bb726
Lucas Victor <lvitors@hotmail.com>
2020-03-10 07:41:09 -0300

    Adicionando condicoes para verificar se o numero eh maior ou menor que o secreto

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,10 +8,16 @@ chute_str = input("Digite um número: ")
 print("Você digitou o numero:", chute_str)
 
 chute = int(chute_str)
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
 
-if(numero_secreto == chute):
+if(acertou):
     print("Você acertou! xD")
 else:
-    print("Você errou! =(")
+    if(maior):
+        print("Você errou! O seu número é maior do que o número secreto. =(")
+    elif(menor):
+        print("Você errou! O seu número é menor do que o número secreto. =(")
 
 print("Fim do jogo!")
