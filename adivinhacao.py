bbdf948328dac134b66d4e7cb466b639d0f96833
Lucas Victor <lvitors@hotmail.com>
2020-03-11 07:49:31 -0300

    Adicionando break e continue dentro do laço do for

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,16 +7,22 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite um número: ")
-    print("Você digitou o numero:", chute_str)
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou o numero", chute_str)
 
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
 
     if(acertou):
         print("Você acertou! xD")
+        break
     else:
         if(maior):
             print("Você errou! O seu número é maior do que o número secreto. =(")
