269a6177fa524c126e54573e8fd406e37a2bb5d6
Lucas Victor <lvitors@hotmail.com>
2020-03-11 07:27:14 -0300

    Aplicando String interpolation

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,7 +7,7 @@ total_de_tentativas = 3
 rodada = 1
 
 while(rodada <= total_de_tentativas):
-    print("Tentativa", rodada, "de", total_de_tentativas)
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite um número: ")
     print("Você digitou o numero:", chute_str)
 
