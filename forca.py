3bd5f61807176a4d5f91f8d73c7469adb95c73ff
Lucas Victor <lvitors@hotmail.com>
2020-03-27 13:29:18 -0300

    Iniciando o jogo da forca

--- forca.py
+++ forca.py
@@ -4,6 +4,21 @@ def jogar():
     print("***Bem vindo ao jogo de forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+        print("jogando...")
+
     print("Fim do jogo!")
 
 if(__name__ == "__main__"):
