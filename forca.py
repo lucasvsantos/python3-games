382806e310c762b3349574d2cbeb1998bd377f0f
Lucas Victor <lvitors@hotmail.com>
2020-04-07 17:48:34 -0300

    Compreensao de lista

--- forca.py
+++ forca.py
@@ -4,8 +4,8 @@ def jogar():
     print("***Bem vindo ao jogo de forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
@@ -25,6 +25,7 @@ def jogar():
                 index += 1
         else:
             erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
 
         enforcou = erros == 6
         acertou = "_" not in letras_acertadas
